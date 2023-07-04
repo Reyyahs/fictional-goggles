a88040ebe4e82f31dabc08d7f62f5b383134c25f
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-07-04 01:36:22 -0400

    added more questions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 //added variable for questions
 var questions = [{
-    q: "What was the top grossing movie made by Hayao Miyazaki:",
+    q: " What was the top grossing movie made by Hayao Miyazaki ? ",
     a: "1. Ponyo",
     b: "2. Howl's Moving Castle",
     c: "3. Princess Mono",
@@ -8,7 +8,7 @@ var questions = [{
     correct: "4. Spirited Away",
 },
 {
-    q:"How many Dragon Balls are there:",
+    q:" How many Dragon Balls are there ? ",
     a: "1. 50 Dragon Balls",
     b: "2. 5 Dragon Balls",
     c: "3. 24 Dragon Balls",
@@ -16,20 +16,37 @@ var questions = [{
     correct: "4. 7 Dragon Balls"
 },
 {
-    q: "In the anime 'Hunter X Hunter, which family is famous for being assassins",
+    q: " In the anime 'Hunter X Hunter, which family is famous for being assassins ? ",
     a: "1. Weston",
     b: "2. Zoldyck",
     c: "3. Khan",
     d: "4. Sans",
-    correct: "2. Zoldyck",
+    correct: "2. Zoldyck"
 
 },
 {
-    q: "Which Anime Protagonist who is half Ghoul and Half Human:",
+    q: " Which Anime Protagonist who is half Ghoul and Half Human ? ",
     a: "1. Naruto ",
     b: "2. Gon",
     c: "3. Kaneki",
     d: "4. Ash",
     correct, "3. Kaneki"
 
+},
+{
+    q: " Who is known as the 'One Punch Man' ? ",
+    a: "1. Naruto ",
+    b: "2. Saitama ",
+    c: "3. Sasuke ",
+    d: "4. Itachi ",
+    correct, "2. Saitama "
+
+},
+{
+    q: "Which of these animes are based on shinobi(Ninjas) ?",
+    a: "1. Naruto ",
+    b: "2. One Piece",
+    c: "3. Hell's Paradise",
+    d: "4. Spy Family",
+    correct, "1. Naruto"
 }]
