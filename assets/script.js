888fa7c0a06301603858b69b7d32165f20da4059
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-07-03 21:13:19 -0400

    added some js with qeustion variable and answers

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+//added variable for questions
+var questions = [{
+    q: "What was the top grossing movie made by Hayao Miyazaki:",
+    a: "1. Ponyo",
+    b: "2. Howl's Moving Castle",
+    c: "3. Princess Mono",
+    d: "4. Spirited Away",
+    correct: "4. Spirited Away",
+},
+{
+    q:"How many Dragon Balls are there:",
+    a: "1. 50 Dragon Balls",
+    b: "2. 5 Dragon Balls",
+    c: "3. 24 Dragon Balls",
+    d: "4. 7 Dragon Balls",
+    correct: "4. 7 Dragon Balls"
+},
+{
+    q: "In the anime 'Hunter X Hunter, which family is famous for being assassins",
+    a: "1. Weston",
+    b: "2. Zoldyck",
+    c: "3. Khan",
+    d: "4. Sans",
+    correct: "2. Zoldyck",
+
+},
+{
+    q: "Which Anime Protagonist who is half Ghoul and Half Human:",
+    a: "1. Naruto ",
+    b: "2. Gon",
+    c: "3. Kaneki",
+    d: "4. Ash",
+    correct, "3. Kaneki"
+
+}]
